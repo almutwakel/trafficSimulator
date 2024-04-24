time step normalization, collision checking, color coding, segment fixes

src/trafficSimulator/core/vehicle.py
@@ -6,12 +6,17 @@ class Vehicle:
         # Set default configuration
         self.set_default_config()
 
+
         # Update configuration
         for attr, val in config.items():
             setattr(self, attr, val)
 
         # Calculate properties
         self.init_properties()
+
+        self.max_stop_timesteps = 50
+        self.stop_timesteps = 0
+        self.collided = False
         
     def set_default_config(self):    
         self.id = uuid.uuid4()
@@ -19,9 +24,12 @@ class Vehicle:
         self.l = 4
         self.s0 = 4
         self.T = 1
-        self.v_max = 16.6
-        self.a_max = 1.44
-        self.b_max = 4.61
+        # self.v_max = 16.6
+        self.v_max = 10
+        # self.a_max = 1.44
+        self.a_max = 0
+        # self.b_max = 4.61
+        self.b_max = 0
 
         self.path = []
         self.current_road_index = 0
@@ -30,6 +38,7 @@ class Vehicle:
         self.v = 0
         self.a = 0
         self.stopped = False
+        self.color = (0, 0, 255)
 
     def init_properties(self):
         self.sqrt_ab = 2*np.sqrt(self.a_max*self.b_max)
@@ -37,23 +46,31 @@ class Vehicle:
 
     def update(self, lead, dt):
         # Update position and velocity
-        if self.v + self.a*dt < 0:
-            self.x -= 1/2*self.v*self.v/self.a
+
+        self.x += self.v*dt
+
+        if self.stopped:
             self.v = 0
         else:
-            self.v += self.a*dt
-            self.x += self.v*dt + self.a*dt*dt/2
+            self.v = self.v_max
+
+        # if self.v + self.a*dt < 0:
+        #     self.x -= 1/2*self.v*self.v/self.a
+        #     self.v = 0
+        # else:
+        #     self.v += self.a*dt
+        #     self.x += self.v*dt + self.a*dt*dt/2
         
-        # Update acceleration
-        alpha = 0
-        if lead:
-            delta_x = lead.x - self.x - lead.l
-            delta_v = self.v - lead.v
+        # # Update acceleration
+        # alpha = 0
+        # if lead:
+        #     delta_x = lead.x - self.x - lead.l
+        #     delta_v = self.v - lead.v
 
-            alpha = (self.s0 + max(0, self.T*self.v + delta_v*self.v/self.sqrt_ab)) / delta_x
+        #     alpha = (self.s0 + max(0, self.T*self.v + delta_v*self.v/self.sqrt_ab)) / delta_x
 
-        self.a = self.a_max * (1-(self.v/self.v_max)**4 - alpha**2)
+        # self.a = self.a_max * (1-(self.v/self.v_max)**4 - alpha**2)
 
-        if self.stopped: 
-            self.a = -self.b_max*self.v/self.v_max
+        # if self.stopped: 
+        #     self.a = -self.b_max*self.v/self.v_max
         